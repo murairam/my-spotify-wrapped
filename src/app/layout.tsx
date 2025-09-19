81f9eb26c2ad62b2b58bf1967a980c3b0e2ab14c
Mari <marimiilpalu@gmail.com>
2025-09-19 17:42:58 +0200

    (add): added vercel analytics comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ReactQueryProvider } from '@/providers/ReactQueryProvider';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <ReactQueryProvider>
           {children}
+          <Analytics />
         </ReactQueryProvider>
       </body>
     </html>
